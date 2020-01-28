61a3830f8675f301f400a67b8a4f2caed7337574
devision1088 <accounts@devision1088.com>
2020-01-27 22:38:33 -0800

    Updated Unillax Container engine
    Updated the Unillax container engine to provide smoother animations.

--- releases/unillax.js
+++ releases/unillax.js
@@ -1,55 +1,29 @@
 (function () { // Making these variables and stuff not global, that would be bad :(
 
 	// Define some global variables that are used everywhere in Unillax
-	var unillaxContainers, unillaxElements;
-
-	// Appends a stylesheet for certain specialty elements
-	function applyStyles() {
-
-		// Creates the stylesheet element
-		var styles = document.createElement("style");
-
-		// Sets the values of the stylesheet to make the unillax-background elements work properly
-		styles.innerHTML = `
-            .unillax-container {
-                -webkit-box-sizing: border-box;
-                -moz-box-sizing: border-box;
-                box-sizing: border-box;
-                overflow: hidden
-            }
-            .unillax-background {
-                width: 100%;
-                height: 100vh;
-                object-fit: cover;
-            }
-            .unillax-overlay {
-                width: 100%;
-                height: 100%;
-                position: relative;
-                margin-top: -100vh;
-            }
-            .unillax-cropped{
-                overflow: hidden;
-            }
-		`;
-
-		// Adds the stylesheet 
-		document.head.appendChild(styles);
-	}
+	var unillaxContainers, unillaxElements, ticking;
 
 	// Updates variables with relevant DOM information
 	function updateDOM() {
-		unillaxContainers = document.getElementsByClassName("unillax-container"), unillaxElements = document.getElementsByClassName("unillax-element");
+		unillaxContainers = document.getElementsByClassName("unillax-container");
+		unillaxElements = document.getElementsByClassName("unillax-element");
+	}
+
+	// If called, an animation frame will be requested if not done so already in the same event
+	function requestTick() {
+		if (!ticking) {
+			requestAnimationFrame(updatePositions);
+		}
+		ticking = true;
 	}
 
 	// Updates positions of all elements
 	function updatePositions() {
 
-		// Sets scroll positions
-		var scrollXPosition = window.pageXOffset;
-		var scrollYPosition = window.pageYOffset;
+		// Resets ticking
+		ticking = false;
 
-		// Sets window dimensions
+		// Updates window size
 		var windowWidth = window.innerWidth;
 		var windowHeight = window.innerHeight;
 
@@ -70,39 +44,55 @@
 
 			// Assign `data-unillax-position-x` to a variable
 			var dataUnillaxPositionX = container.getAttribute("data-unillax-position-x");
+			if (dataUnillaxPositionX) {
+				var unillaxPositionX = dataUnillaxPositionX;
+			} else {
+				var unillaxPositionX = 0;
+			}
+
 			// Assign `data-unillax-position-y` to a variable
 			var dataUnillaxPositionY = container.getAttribute("data-unillax-position-y");
+			if (dataUnillaxPositionY) {
+				var unillaxPositionY = dataUnillaxPositionX;
+			} else {
+				var unillaxPositionY = 0;
+			}
 
 			// Assign `data-unillax-depth` to a variable
 			var dataUnillaxDepth = container.getAttribute("data-unillax-depth");
-
-			// Sets depth
 			if (dataUnillaxDepth) {
 				var depth = dataUnillaxDepth;
 			} else {
-				var depth = 1;
+				var depth = 2;
 			}
 
 			// Calculates final transformations
-			var transformX = scrollXPosition / depth;
-			var transformY = -elementOffsetTop / depth;
+			var transformX = (-elementOffsetLeft / depth) + (unillaxPositionX / 2) * -(windowWidth - elementWidth);
+			var transformY = (-elementOffsetTop / depth) + (unillaxPositionY / 2) * (windowHeight - elementHeight);
+
+			// Calculates final transformations for backgrounds elements
+			var transformBackgroundX = (-elementOffsetLeft / depth);
+			var transformBackgroundY = (-elementOffsetTop / depth);
 
 			// Transforms children
 			for (var j = 0; j < children.length; j++) {
-				children[j].style.transform = "translate(" + transformX + "px, " + transformY + "px)";
+				if (children[j].className !== "unillax-background") {
+					children[j].style.transform = "translate(" + transformX + "px, " + transformY + "px)";
+				} else if (children[j].className === "unillax-background") {
+					children[j].style.transform = "translate(" + transformBackgroundX * 1.5 + "px, " + transformBackgroundY * 1.5 + "px)";
+				}
 			}
-
 		}
-		window.requestAnimationFrame(updatePositions);
 	}
 
-	function updateAnimation() {
-		window.requestAnimationFrame(updatePositions);
-	}
-	// Initially updates DOM variables
-	applyStyles();
+	// Updates the variables that directly access the DOM fist
 	updateDOM();
 
-	window.addEventListener("scroll", updateAnimation);
+	// Adds event listeners to request tick
+	window.addEventListener("scroll", requestTick);
+	window.addEventListener("resize", requestTick);
+
+	// Calls animation frame once to start
+	requestAnimationFrame(updatePositions);
 
 })();
